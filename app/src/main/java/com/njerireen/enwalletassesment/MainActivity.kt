3ea2034626fd5b1609508d567aca90493c01352d
Maureen-Njeri <maureenwangarinjeri@gmail.com>
2024-07-17 13:50:19 +0300

    Add EnWallet Assesment

--- app/src/main/java/com/njerireen/enwalletassesment/MainActivity.kt
+++ app/src/main/java/com/njerireen/enwalletassesment/MainActivity.kt
@@ -5,16 +5,32 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.njerireen.enwalletassesment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.rvExpenses.layoutManager = LinearLayoutManager(this)
+        displayExpense()
+    }
+
+    fun displayExpense(){
+        val expense1 = Wallet("Salary", "KES 40000", "1 July 2024", "Rent","16000", "2 July 2024")
+        val expense2 = Wallet("Dividends", "KES 2400", "4 July 2024", "Rent","16000", "5 July 2024")
+        val expense3 = Wallet("Internet", "KES 2500", "5 July 2024", "Electricity","800", "5 July 2024")
+        val expense4 = Wallet("Bus Fare", "KES 500", "11 July 2024", "Shopping","3500", "12 July 2024")
+        val expense5 = Wallet("Dividends", "KES 2400", "4 July 2024", "Rent","16000", "5 July 2024")
+        val expense6 = Wallet("Internet", "KES 2500", "5 July 2024", "Electricity","800", "5 July 2024")
+        val expense7 = Wallet("Bus Fare", "KES 500", "11 July 2024", "Shopping","3500", "12 July 2024")
+        val expense8 = Wallet("Dividends", "KES 2400", "4 July 2024", "Rent","16000", "5 July 2024")
+        val expense9 = Wallet("Salary", "KES 40000", "1 July 2024", "Rent","16000", "2 July 2024")
+
+        val expenseList = listOf(expense1,expense2,expense3,expense4, expense5, expense6, expense7, expense8, expense9 )
+        val expenseAdapter = WalletAdapter(expenseList)
+        binding.rvExpenses.adapter = expenseAdapter
     }
 }
---
+++ app/src/main/java/com/njerireen/enwalletassesment/Wallet.kt
@@ -0,0 +1,10 @@
+package com.njerireen.enwalletassesment
+
+data class Wallet(
+    var category:String,
+    var amount: String,
+    var date:String,
+    var category2:String,
+    var amount2: String,
+    var date2:String
+)
